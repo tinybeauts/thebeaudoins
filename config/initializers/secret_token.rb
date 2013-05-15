82d4f5dd413fb477fdc1f4f972fce5f7c00abf66
Julianne Beaudoin <jubeaudoin@gmail.com>
2013-05-15 15:43:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebeaudoins::Application.config.secret_token = '5ce963246df5502395ed79a43e4736bc183b10ffe8351695a8dce7c61327e5ef2a9740ef53419cc156d59009e2da03d7c52e252b0f4501c2ff35d2b52d05f245'
